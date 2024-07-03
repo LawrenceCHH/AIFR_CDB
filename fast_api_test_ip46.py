1bc8fe9ae434b2a6d11b7799b36edc46b1caa94b
Lawrence_w01 <lawrencechh.cs@gmail.com>
2024-07-04 01:25:21 +0800

    add backend fastapi

--- fast_api_test_ip46.py
+++ fast_api_test_ip46.py
@@ -11,6 +11,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
 from fastapi_pagination import Page, add_pagination, paginate
+from fastapi.middleware.cors import CORSMiddleware
 
 # class JSONAPIParams(BaseModel, AbstractParams):
 #     page: int = Query(1, ge=1, description="Page number")
@@ -127,7 +128,19 @@ class JSONAPIPage(AbstractPage[T], Generic[T]):
             **kwargs,
         )
 
+
 app = FastAPI()
+
+origins = [
+    '*',
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class UserOut(BaseModel):
     name: str
@@ -135,25 +148,26 @@ class UserOut(BaseModel):
 
 users: List[UserOut] = [UserOut(name="John Doe", email="john-doe@example.com")]
 
+@app.get("/")
+async def index_html():
+    return {'Response': 'Port 6128 is set up successfully on w02'}
+# @app.get("/paginate", response_model=JSONAPIPage[UserOut])
 
 @app.get("/users", response_model=JSONAPIPage[UserOut])
 async def get_users(data, params: JSONAPIParams = Depends()) -> JSONAPIPage[UserOut]:
 
     return paginate(data, params=params)
 
-# @app.get("/paginate", response_model=JSONAPIPage[UserOut])
-@app.get("/paginate")
-# async def paginate_user(page: int, size: int) -> JSONAPIPage[UserOut]:
-async def paginate_user(page: int, size: int):
-    params = JSONAPIParams(page=page, size=size)
-    result_dict = {'1':"", '2':"", '3':""}
-    for i in range(3):
-        res = await get_users(users, params)
-        res_dict = dict(res)
-        result_dict[str(i)] = res_dict
-        print(res_dict)
-    return result_dict
 
-add_pagination(app)
-
-
+import uvicorn
+if __name__ == '__main__':
+    # uvicorn.run('cdb_api:app', host="127.0.0.1", port=6128)
+    uvicorn.run('pagination_test:app', host="0.0.0.0", port=6128)
+    # uvicorn.run('pagination_test:app', host="127.0.0.1", port=6128)
+    # uvicorn.run('pagination_test:app', host="0.0.0.0", port=6128)
+    # pm2 start pagination_test.py --name test --interpreter=/cluster/home/lawrencechh.cs/miniconda3/envs/chatglm2_env/bin/python
+    # print(domain_setting['host'])
+    # # Formal server
+    # uvicorn.run('cdb_api:app', host=domain_setting['host'], port=domain_setting['port'], forwarded_allow_ips='*')
+    # uvicorn pagination_test:app --host 127.0.0.1 --port 6128
+    # uvicorn pagination_test:app --host 0.0.0.0 --port 6128
