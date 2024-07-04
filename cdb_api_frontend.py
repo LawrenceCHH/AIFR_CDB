dc81d5ddbc8bfc94bff0b696f826495f8d5a8f66
Lawrence-AIFR-public <lawrencechh.cs@gmail.com>
2024-07-04 15:44:14 +0800

    add frontend request to w01

--- cdb_api_frontend.py
+++ cdb_api_frontend.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 # Cross origin and allow origins
 from fastapi.middleware.cors import CORSMiddleware
+from aiohttp import ClientSession
 
 # Lifespan
 from contextlib import asynccontextmanager
@@ -37,6 +38,7 @@ origins = [
     # '*',
     '127.0.0.1',
     '140.114.80.195',
+    '140.114.80.46',
     'https://jerry914.github.io',
     'https://jerry914.github.io/ai-annotated-judgment-database',
 ]
@@ -49,29 +51,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+@app.get("/api/search")
+# 輸入所需的參數
+async def search(request: Request):
+    payload = dict(request.query_params)
+    print('>>>payload:', payload)
+    w01_url = 'http://140.114.80.46:6128/'
+    
+    async with ClientSession() as sess:
+        async with sess.get(w01_url, params=payload) as response:
+            if response.status == 200:
+                # return await response.json(content_type=None)
+                return await response.text()
+            else:
+                raise HTTPException(status_code=response.status, detail="Failed to retrieve data")
 from fastapi.staticfiles import StaticFiles
 
-frontend_template_dir = '/home/lawrencechh/AIFR_CDB/frontend_deployment/20240704_dist'
+# frontend_template_dir = '/home/lawrencechh/AIFR_CDB/frontend_deployment/20240704_dist'
 
 
-app.mount('/', StaticFiles(directory=frontend_template_dir, html=True), name='ai-annotated-judgment-database')
+# app.mount('/', StaticFiles(directory=frontend_template_dir, html=True), name='ai-annotated-judgment-database')
 
-@app.exception_handler(404)
-async def redirect_all_requests_to_frontend(request: Request, exc: HTTPException):
+# @app.exception_handler(404)
+# async def redirect_all_requests_to_frontend(request: Request, exc: HTTPException):
 
-    request_url = str(request.url)
-    splitted_url = request_url.split('/')[3]
-    splitted_url = 'search-result?' if splitted_url.startswith('search-result?') else splitted_url
-    vue_router_paths = ['about', 'search-result?', 'members']
-    path_validated = splitted_url in vue_router_paths
-    if path_validated:
-        return HTMLResponse(open(frontend_template_dir+"/index.html").read())
-    else:
-        return JSONResponse({"detail":"Not Found"})
+#     request_url = str(request.url)
+#     splitted_url = request_url.split('/')[3]
+#     splitted_url = 'search-result?' if splitted_url.startswith('search-result?') else splitted_url
+#     vue_router_paths = ['about', 'search-result?', 'members']
+#     path_validated = splitted_url in vue_router_paths
+#     if path_validated:
+#         return HTMLResponse(open(frontend_template_dir+"/index.html").read())
+#     else:
+#         return JSONResponse({"detail":"Not Found"})
 
 import uvicorn
-domain_setting = {'host': '127.0.0.1', 'port': 6128}
+# domain_setting = {'host': '127.0.0.1', 'port': 6128}
+domain_setting = {'host': '127.0.0.1', 'port': 6130}
 domain = f"http://{domain_setting['host']}:{domain_setting['port']}" + '/'
 
 if __name__ == '__main__':
